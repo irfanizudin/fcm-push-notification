d5f2e89aa87859f9251e066f369b3bed4e6fd7e6
irfanizudin <irfanizzudyn@gmail.com>
2023-04-30 12:10:52 +0700

    Initial Commit

---
+++ pushNotification/pushNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  pushNotificationApp.swift
+//  pushNotification
+//
+//  Created by Irfan Izudin on 30/04/23.
+//
+
+import SwiftUI
+
+@main
+struct pushNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
